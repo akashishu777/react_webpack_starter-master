2b7f1d5865a2bf37b24e9cffe0cfdcd3b7d0d0bc
Akash Vishwakarma <14cse031giet@gmail.com>
2018-05-03 12:19:16 +0530

    React starter master
    React with webpack master starter

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My React App!</h1>
+        <h1>running</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
