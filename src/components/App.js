2125ec6097b48c1da85501323d6070f9ec357f43
Akash Vishwakarma <14cse031giet@gmail.com>
2018-05-27 22:58:57 +0530

    Dynamic
    Dynamic value change through state variable

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+	getVal()
+	{ 
+		return "Will";
+	}
+	
+
   render() {
     return (
       <div>
         <h1>My React App!</h1>
-        <h1>Running</h1>
+        <h1>Hello {this.getVal()}</h1>
       </div>
     );
   }
